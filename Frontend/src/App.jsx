b20bc762a39ef608f77a9045c3e2c2cb216e0621
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-06-04 16:09:19 +0200

    Creación del redireccionamiento
    Creación del redireccionamiento en App.jsx mediante React Router DOM

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import './App.css'
 import HomePage from './pages/HomePage'
+import ErrorPage from "./pages/ErrorPage";
+import TestPage from './testConnection/Test'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import AppHomePage from "./appPages/AppHomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const debug = import.meta.env.VITE_DEBUG_LOG
+  const shouldShowLayout = location.pathname.startsWith('/app');
 
-  return (
-    <>
-      <HeaderComponent/>
+  return (<>
+    {!shouldShowLayout && <HeaderComponent/>}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/connection" element={debug && <TestPage/>}/>
+        <Route path="/app/main" element={<AppHomePage/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+    </Router>
+    {!shouldShowLayout && <FooterComponent />}
+  </>);
 
-      <HomePage/>
-
-      <FooterComponent/>
-    </>
-  )
 }
+
 
 export default App
---
+++ Frontend/src/appPages/AppHomePage.jsx
@@ -0,0 +1,11 @@
+import '../appStyles/AppHomePage.css'
+
+function AppHomeComponent() {
+    return(
+        <>
+            <h1>Estás en la página principal de la aplicación</h1>
+        </>
+    );
+}
+
+export default AppHomeComponent;
