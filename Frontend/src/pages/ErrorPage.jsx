839837b69524d671af38b19d2c243c2b490c9125
David <morenogarcia.caro@gmail.com>
2025-06-09 13:27:14 +0200

    Cambios peligrosos

---
+++ Frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import '../styles/ErrorPage.css'
+
+function ErrorPage() {
+
+    const uri = location.pathname;
+
+    return (
+        <>
+        <h1>Estás viendo la página de Error</h1>
+        <h2>Porque la página: {uri} no existe</h2>
+        </>
+    )
+}
+
+export default ErrorPage
