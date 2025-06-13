99462a519b7e6fa095b6161fdd2048dc6652f116
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-06-13 22:22:30 +0200

    Creados los filtros y la barra de busqueda
    Se han creado los filtros y la barra de búsqueda que se usarán más adelante

---
+++ Frontend/src/appComponents/AppFilterDateComponent.jsx
@@ -0,0 +1,36 @@
+import '../appStyles/AppFilterDateComponent.css'
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useLanguage } from '../context/LanguageContext';
+
+function AppFilterDateComponent() {
+
+    const {currentTexts} = useLanguage();
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+
+
+    return(
+        <div id='FilterDate_container'>
+            <h2 id="FilterDate_title">{currentTexts.dateFilter.title}</h2>
+            <DatePicker
+                className="FilterDate_DatePicker"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="MM/yyyy" // Formato de mes/año
+                showMonthYearPicker // Muestra solo mes y año
+                placeholderText={currentTexts.dateFilter.startDate}
+            />
+            <DatePicker
+                className="FilterDate_DatePicker"
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+                dateFormat="MM/yyyy" // Formato de mes/año
+                showMonthYearPicker // Muestra solo mes y año
+                placeholderText={currentTexts.dateFilter.endDate}
+            />
+        </div>
+    )
+}
+export default AppFilterDateComponent
